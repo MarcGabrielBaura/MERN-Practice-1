86af7d09d10f3cd4815d91a3fded97415587ec76
MarcBaura <marcbaura@gmail.com>
2023-01-17 20:31:28 +0800

    Auth

--- server/index.js
+++ server/index.js
@@ -1,26 +1,28 @@
-import express from "express"
-import bodyParser from "body-parser"
-import mongoose from "mongoose"
-import cors from "cors"
-import dotenv from "dotenv"
-import multer from "multer"
-import helmet from "helmet"
-import morgan from "morgan"
-import path from "path"
-import { fileURLToPath } from "url"
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js";
+import { register } from "./controllers/auth.js";
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-dotenv.config()
-const app = express()
-app.use(express.json)
-app.use(helmet())
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin"}))
-app.use(morgan("common"))
-app.use(bodyParser.json({ limit: "30mb", extended: true }))
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
-app.use(cors())
-app.use("/assets", express.static(path.join(__dirname, 'public/assets')))
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+app.use(express.json);
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin"}));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
@@ -29,15 +31,17 @@ const storage = multer.diskStorage({
     filename: function(req, file, cb) {
         cb(null, file.originalname)
     }
-})
+});
 
-const upload = multer({ storage })
+const upload = multer({ storage });
 
-mongoose.set('strictQuery', false)
+app.post("/auth/register", upload.single("picture"), register);
+app.use("/auth", authRoutes);
+mongoose.set('strictQuery', false);
 const PORT = process.env.PORT || 6001
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
-    app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
-}).catch((error) => console.log(`${error} did not connect`))
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+}).catch((error) => console.log(`${error} did not connect`));
